9b23b7dfe4592760ba7d32fb10eaae0f3a964f08
ravirajthe <ravirajthechamp@gmail.com>
2017-07-23 22:40:11 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var x = ["ravi","push","manish","ravi"];
+var count = 0;
+for (var i = x.length-1; i>=0; i--) {
+	console.log(x[i]);
+}
+
+console.log("##############################");
+while(count<x.length){
+
+	console.log(x[count]);
+	count+=2;
+
+}
